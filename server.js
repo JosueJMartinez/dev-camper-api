b47e794807fe533bd59abb4b096800dc3c28fa8b
JosueJMartinez <jmarti61@gmail.com>
2021-03-24 00:50:15 +0000

    Updated with first route sending json

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ const express = require('express'),
 // Load config file for env vars
 dotenv.config({path:'./config/config.env'});
 
-const PORT = process.env.PORT || 5001;
-const NODE_ENV = process.env.NODE_ENV;
 
+const {PORT, NODE_ENV} = {...process.env}
 console.log(PORT)
 const app = express();
 
 app.get('/', (req,res)=>{
-	console.log('in root');
-	res.send('hello world');
+	res.status(400).json({first_name:'Josue', last_name:'Martinez'});
 })
 
 app.listen(PORT,()=>{
