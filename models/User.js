d76865ba261b926c6a3bea1805b276362bf71c7f
JosueJMartinez <jmarti61@gmail.com>
2021-04-12 13:21:04 -0500

    started working on JWT token

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const opts = { toJSON: { virtuals: true }, toObject: { virtuals: true } };
 
@@ -46,5 +47,8 @@ UserSchema.pre('save', async function (next) {
 	const salt = await bcrypt.genSalt(10);
 	this.password = await bcrypt.hash(this.password, salt);
 });
+
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function () {};
 
 module.exports = mongoose.model('User', UserSchema);
