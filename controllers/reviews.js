2c36754f59bae96ff062a8f2c6a898d3d9d94886
Josue Martinez <josue.martinez@cdw.com>
2021-04-23 22:15:27 -0500

    working on the crud for reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -13,7 +13,15 @@ exports.getReviews = asyncHandler(async (req, res, next) => {
 	// let query;
 	const { bootcampId } = { ...req.params };
 	if (bootcampId) {
-		const reviews = await Review.find({ bootcamp: bootcampId });
+		const reviews = await Review.find({ bootcamp: bootcampId })
+			.populate({
+				path: 'bootcamp',
+				select: 'name description',
+			})
+			.populate({
+				path: 'user',
+				select: 'name',
+			});
 		res.status(200).json({
 			success: true,
 			count: reviews.length,
@@ -22,4 +30,85 @@ exports.getReviews = asyncHandler(async (req, res, next) => {
 	} else {
 		res.status(200).json(res.advancedResults);
 	}
+});
+
+//  @desc     Get a single review
+//  @route    Get /api/v1/reviews/reviewId
+//  @access   Public
+exports.getReview = asyncHandler(async (req, res, nexr) => {
+	const { reviewId } = { ...req.params };
+	const review = await Review.findById(reviewId)
+		.populate({
+			path: 'bootcamp',
+			select: 'name description',
+		})
+		.populate({
+			path: 'user',
+			select: 'name',
+		});
+
+	if (!review)
+		throw new ErrorResponse(
+			`1. Resource not found with id of ${reviewId}`,
+			404,
+			reviewId
+		);
+
+	res.status(200).json({
+		success: true,
+		data: review,
+	});
+});
+
+//  @desc     Add a review
+//  @route    Post /api/v1/bootcamps/:bootcampId/reviews
+//  @access   Private
+exports.createReview = asyncHandler(async (req, res, next) => {
+	const { bootcampId } = { ...req.params };
+	const currentUser = req.user;
+
+	const foundBootCamp = await Bootcamp.findById(bootcampId);
+	const foundUserReview = await Review.findOne({
+		user: currentUser.id,
+		bootcamp: bootcampId,
+	});
+
+	// check for bootcamp exists before creating the review
+	if (!foundBootCamp) {
+		throw new ErrorResponse(
+			`Resource not found with id of ${bootcampId}`,
+			404,
+			bootcampId
+		);
+	}
+
+	// Check if user already made a review for this bootcamp
+	if (foundUserReview) {
+		throw new ErrorResponse(
+			`Already made a review for this bootcamp`,
+			401
+		);
+	}
+
+	// Make sure user is owner of bootcamp to create course
+	if (
+		foundBootCamp.user.toString() !== currentUser.id &&
+		currentUser.role !== 'admin'
+	)
+		throw new ErrorResponse(
+			`User ${req.user.id} is not authorized to create course for bootcamp ${bootcampId}`,
+			401
+		);
+
+	const newCourse = new Course({
+		bootcamp: bootcampId,
+		user: req.user.id,
+		...req.body,
+	});
+	const addedCourse = await newCourse.save();
+
+	res.status(201).json({
+		success: true,
+		data: addedCourse,
+	});
 });
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const { getReviews } = require('../controllers/reviews');
+const { getReviews, getReview } = require('../controllers/reviews');
 const Review = require('../models/Review');
 const advancedResults = require('../middleware/advancedResults');
 const router = express.Router({ mergeParams: true });
 const { protect, authorize } = require('../middleware/auth');
 
-// router
-// 	.route('/:courseId')
-// 	.get(getCourse)
+router.route('/:reviewId').get(getReview);
 // 	.put(protect, authorize('publisher', 'admin'), updateCourse)
 // 	.delete(protect, authorize('publisher', 'admin'), deleteCourse);
 router.route('/').get(
